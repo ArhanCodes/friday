b4e5810dac232facec57d7a14147640bf3469b27
arhan <arhanh1234@gmail.com>
2026-02-27 17:12:59 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/friday/dist/',
+  base: '/friday/',
   plugins: [react(), tailwindcss()],
 })
